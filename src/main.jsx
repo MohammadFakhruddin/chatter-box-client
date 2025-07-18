917da565da6892215030730f23b4ffe20283b532
Mohammad Fakhruddin <mohammadfakhruddin131@gmail.com>
2025-07-18 14:32:38 +0600

    main file created

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { RouterProvider } from 'react-router'
+import { router } from './Routes/Routes'
+import AuthProvider from './Provider/AuthProvider'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <AuthProvider>
 
+      <div className='font-manrope'>
+        <RouterProvider router={router}></RouterProvider>
+
+      </div>
+    </AuthProvider>
 
   </StrictMode>,
 )

ba605e97a532f6d42d3e64849253ccacab4914c3
Mohammad Fakhruddin <mohammadfakhruddin131@gmail.com>
2025-07-18 14:29:53 +0600

    created routes

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -11,6 +11,12 @@ import Dashboard from '../Layout/Dashboard';
 import MyProfile from '../Pages/MyProfile';
 import AddPost from '../Pages/AddPost';
 import MyPosts from '../Pages/MyPosts';
+import Membership from '../Pages/Membership';
+import CommentsPage from '../Pages/CommentsPage';
+import AdminDashboard from '../Layout/adminDashboard';
+import ManagesUsers from '../Pages/ManagesUsers';
+import ReportedActivities from '../Pages/ReportedActivities';
+import AdminProfile from '../Pages/AdminProfile';
 
 export const router = createBrowserRouter([
   {
@@ -19,50 +25,78 @@ export const router = createBrowserRouter([
     children: [
       {
         path: '/',
-       element: <Home/>,
+        element: <Home />,
       },
+
       {
-        path:'/make-announcement',
-        element:<MakeAnnouncement></MakeAnnouncement>
-      },
-      {
-        path:'/auth',
-        element:<AuthLayout/>,
-        children:[
+        path: '/auth',
+        element: <AuthLayout />,
+        children: [
           {
-            path:'/auth/login',
-            element:<Login/>
+            path: '/auth/login',
+            element: <Login />
           },
           {
-            path:'/auth/register',
-            element:<Register/>
+            path: '/auth/register',
+            element: <Register />
           }
         ]
       },
       {
-        path:'/post/:id',
-        element:<PostDetails/>,
+        path: '/post/:id',
+        element: <PostDetails />,
       },
       {
-        path:'/payment-success',
-        element:<PaymentSuccess/>
+        path: '/membership',
+        element: <Membership></Membership>
+
       },
       {
-        path:'/dashboard',
-        element:<Dashboard/>,
-        children:[
+        path: '/payment-success',
+        element: <PaymentSuccess />
+      },
+      {
+        path: '/dashboard',
+        element: <Dashboard />,
+        children: [
           {
-            path:'/dashboard/myprofile',
-            element:<MyProfile/>
+            path: '/dashboard/myprofile',
+            element: <MyProfile />
           },
           {
-            path:'/dashboard/addpost',
-            element:<AddPost/>
+            path: '/dashboard/addpost',
+            element: <AddPost />
           },
           {
-            path:'/dashboard/myposts',
-            element:<MyPosts/>
-          }
+            path: '/dashboard/myposts',
+            element: <MyPosts />
+          },
+        ]
+      },
+      {
+        path: '/comments/:id',
+        element: <CommentsPage></CommentsPage>
+      },
+      {
+        path: '/admin-dashboard',
+        element: <AdminDashboard></AdminDashboard>,
+        children: [
+          {
+            path: '/admin-dashboard/manage-users',
+            element: <ManagesUsers></ManagesUsers>
+          },
+          {
+            path: '/admin-dashboard/reported-activities',
+            element: <ReportedActivities></ReportedActivities>
+          },
+          {
+            path: '/admin-dashboard/admin-profile',
+            element: <AdminProfile></AdminProfile>
+          },
+          {
+            path: '/admin-dashboard/make-announcement',
+            element: <MakeAnnouncement></MakeAnnouncement>
+          },
         ]
       }
     ],
